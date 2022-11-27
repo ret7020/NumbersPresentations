0ad1db7b2e8c2183342665acaec13bf338fd846f
ret7020 <ret7020@gmail.com>
2022-11-27 20:42:31 +0300

    Possitive && no shift case

--- main.cpp
+++ main.cpp
@@ -17,11 +17,29 @@ int dec2bin(int num)
 }
 
 int main(){
+    // CONFIG PART
+    bool DEBUG = true;
+    //
     ll number, base_size = 8;
     cout << "Enter number: ";
     cin >> number;
     cout << "Enter size: ";
     cin >> base_size;
-    cout << "Storing " << number << "in size " << base_size << "\n";
+    if(DEBUG) cout << "Storing " << number << "in size " << base_size << "\n";
     ll res_bin =  dec2bin(number);
+    if (DEBUG) cout << "Binary repr: " << res_bin << "\n";
+    string res = "";
+    if (number > 0){
+        if (DEBUG) cout << "Possitive number";
+        res += "0";
+        string bin_repr = to_string(res_bin);
+        ll shift_size = bin_repr.length() - 8 + 1;
+        cout << "Shift number for " << shift_size;
+        if (shift_size == 0){ // No shift
+            res += bin_repr;
+        }
+        cout << "Direct code: " << res;
+    }else{
+
+    }
 }
