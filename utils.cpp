ab6d6541681307b77dad6c279f03fc1b14fe60a1
Stephan Zhdanov <55328925+ret7020@users.noreply.github.com>
2022-12-13 11:08:53 +0300

    Failsafe mode for printing

--- utils.cpp
+++ utils.cpp
@@ -3,15 +3,17 @@
 #include "utils.h"
 
 string generate_colored(string base, int color_id){
+    bool FAILSAFE = false;
+    if (FAILSAFE) return base;    
     string res = "";
     switch (color_id){
         case 0: // red color
             res = "\x1B[31m" + base + "\033[0m\t\t";
             break;
-        case 1: // cyan
+        case 1: // cyan color
             res = "\x1B[36m" + base + "\033[0m\t\t";
             break;
-        case 2:
+        case 2: // green color
             res = "\x1B[32m" + base + "\033[0m\t\t";
             break;
     }
